b0c6635872dbf4d79f0dd8e1c16724021605ec5d
Rahul Desar <desarrahul@gmail.com>
2024-03-30 18:16:31 +0545

    basic server listening on 5566

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:5566").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(_) => {
+                println!("Connection Successful");
+            }
+            Err(_) => todo!(),
+        }
+    }
 }
