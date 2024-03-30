87f084c7b874a6e3e68f7220883eb4461b89a6cf
Rahul Desar <desarrahul@gmail.com>
2024-03-30 18:43:46 +0545

    add: dummy html response

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{
-    io::{BufRead, BufReader},
+    fs,
+    io::{BufRead, BufReader, Write},
     net::{TcpListener, TcpStream},
 };
 
@@ -25,4 +26,11 @@ fn handle_connection(mut stream: TcpStream) {
         .collect();
 
     println!("Request: {:#?}", http_request);
+
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let length = contents.len();
+
+    let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    stream.write_all(response.as_bytes()).unwrap();
 }
